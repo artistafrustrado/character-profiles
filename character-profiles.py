54c00a620d4f1f36d87b3b4e1329c1ccfe95343f
Fernando Michelotti <artista.frustrado@gmail.com>
2021-04-15 02:40:26 -0300

    Finished character's form

--- character-profiles.py
+++ character-profiles.py
@@ -12,122 +12,202 @@ from PySide6 import QtCore, QtWidgets, QtGui
 
 from exportbs import *
 
-# https://www.pythoncentral.io/pyside-pyqt-tutorial-the-qlistwidget/
-# https://www.youtube.com/watch?v=m_LXqG8VoDA
-# https://www.youtube.com/watch?v=Sl-5-gnn8mk 
-# https://www.youtube.com/channel/UCPme28sMOcWS50CgtTWUZIw
-#  https://www.pythoncentral.io/pyside-pyqt-tutorial-qlistview-and-qstandarditemmodel/
-#  https://github.com/tonytony2020/Qt-Python-Binding-Examples/blob/master/list_tree_and_table/listview_dnd/listview.py
-# http://www2.hawaii.edu/~takebaya/cent110/gui/qtablewidget.html
-# https://realpython.com/python-pyqt-database/
-# https://www.daniweb.com/programming/software-development/threads/468891/displaying-images-in-qlistview-with-same-thumbnail-size-keeping-aspect-rat
-# https://www.programcreek.com/python/example/108101/PyQt5.QtWidgets.QListView
-# https://www.learnpyqt.com/tutorials/modelview-architecture/
-# ui->tableView->setModel(model);
-# ui->tableView->setColumnHidden(5, true);
+class CharacterWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self._setupUI()
 
-# https://linuxhint.com/use-pyqt-qtablewidget/
-# https://gist.github.com/345161974/dd5003ed9b706adc557ee12e6a344c6e
+    def _setupUI(self):
+        hbox = QHBoxLayout()
+        vbox = QVBoxLayout()
+#        hbox.addLayout(vbox)
+        
+        label = QLabel("Character")
+        label.setAlignment(Qt.AlignCenter)
+        label.setObjectName('CharacterList')
+        vbox.addWidget(label)
 
+        fbox = QFormLayout()
 
-class TableModel(QtCore.QAbstractTableModel):
-    def __init__(self, data):
-        super(TableModel, self).__init__()
-        self._data = data
+        self.name = QLineEdit()
+        self.age = QLineEdit()
 
-    def data(self, index, role):
-        if role == Qt.DisplayRole:
-            # See below for the nested-list data structure.
-            # .row() indexes into the outer list,
-            # .column() indexes into the sub-list
-            return self._data[index.row()][index.column()]
+        self.gender = QComboBox(self)
+        self.gender.addItem("Male")
+        self.gender.addItem("Female")
+        self.gender.addItem("Gay")
+        self.gender.addItem("Lesbian")
+        self.gender.addItem("Trans Man")
+        self.gender.addItem("Trans Woman")
+        self.gender.addItem("Intersex")
 
-    def rowCount(self, index):
-        # The length of the outer list.
-        return len(self._data)
+        self.function = QComboBox(self)
+        self.function.addItem("Primary")
+        self.function.addItem("Secondary")
+        self.function.addItem("Extra")
+        
+        self.physicalDescription = QTextEdit()
+        self.mbty = QTextEdit()
+        
+        self.fear = QLineEdit()
+        self.motivation = QLineEdit()
+        self.kryptonite = QLineEdit()
+        self.misbelief = QLineEdit()
+        self.lesson = QLineEdit()
+        self.bestThing = QLineEdit()
+        self.worstThing = QLineEdit()
+        self.lookDown = QLineEdit()
+        self.feelAlive = QLineEdit()
+        self.feelLoved = QLineEdit()
+        self.valueMost = QLineEdit()
+        self.favorites = QLineEdit()
+        self.objectPartWith = QLineEdit()
+        self.typicalOutfit = QLineEdit()
+        self.nicknames = QLineEdit()
+        self.methodManipulation = QLineEdit()
+        self.dailyRoutine = QLineEdit()
+        self.gotoCure = QLineEdit()
+        self.dissatisfaction = QLineEdit()
+        self.beliefHappyness = QLineEdit()
+        self.turnDreamReality = QLineEdit()
+        self.feelAccomplishDream = QLineEdit()
+#        self.backstoryChangedEverything = QLineEdit()
+        self.beliefChange = QLineEdit()
+        self.struggleHoldBelief = QLineEdit()
+        self.newBelief = QLineEdit()
 
-    def columnCount(self, index):
-        # The following takes the first sub-list, and returns
-        # the length (only works if all rows are an equal length)
-        return len(self._data[0])
+        fbox.addRow(QLabel("Name"), self.name)
+        fbox.addRow(QLabel("Age"), self.age)
+        fbox.addRow(QLabel("Gender"), self.gender)
+        fbox.addRow(QLabel("Function"), self.function)
+        fbox.addRow("Physical Description", self.physicalDescription)
+        fbox.addRow("MBTY / Enneagram Personality Type", self.mbty)
+#        vbox.addWidget(fbox)
+#        vbox.addWidget(QLabel("Label"))
+        
+        fbox1 = QFormLayout()
+#        fbox1.addRow("Physical Description", self.physicalDescription)
+#        fbox1.addRow("MBTY / Enneagram Personality Type", self.mbty)
 
-    def setTableData(self):
-        for column, key in enumerate(self._data):
-            for row, item in enumerate(self._data[key]):
-                newitem = QTableWidgetItem(item)
-                self.setItem(row, column, newitem)
+        grp01 = QGroupBox('Character | main aspects')
+        grp01.setLayout(fbox)
+        vbox.addWidget(grp01)
+        
+        grp02 = QGroupBox('Internal')
+        fbox1.addRow("Greatest Fear", self.fear)
+        fbox1.addRow("Motivation", self.motivation)
+        fbox1.addRow("Kryptonite", self.kryptonite)
+        fbox1.addRow("Misbelief about the world", self.misbelief)
+        fbox1.addRow("Lesson needed to learn", self.lesson)
+        fbox1.addRow("Best thing in life", self.bestThing)
+        fbox1.addRow("Worst thing in life", self.worstThing)
+        fbox1.addRow("What thing she looks down for", self.lookDown)
+        fbox1.addRow("What makes them feel alive", self.feelAlive)
+        fbox1.addRow("What makes them feel loved", self.feelLoved)
+        fbox1.addRow("What are the three things value the most", self.valueMost)
+        grp02.setLayout(fbox1)
+        grp02.setLayout(fbox1)
+        
+        fbox2 = QFormLayout()
+        grp03 = QGroupBox('External')
+        fbox2.addRow("Favorite book, movie, and band", self.favorites)
+        fbox2.addRow("Object can't bear part with? why?", self.objectPartWith)
+        fbox2.addRow("Typical Outfit", self.typicalOutfit)
+        fbox2.addRow("Nicknames", self.nicknames)
+        fbox2.addRow("Method of manipulation", self.methodManipulation)
+        fbox2.addRow("Daily Routine", self.dailyRoutine)
+        fbox2.addRow("Go to cure for a bad day", self.gotoCure)
+        grp03.setLayout(fbox2)
+        
+        fbox3 = QFormLayout()
+        grp04 = QGroupBox('Goals')
+        
+        fbox3.addRow("Dissatisfaction", self.dissatisfaction)
+        fbox3.addRow("Belief that brings happyness", self.beliefHappyness)
+        fbox3.addRow("Turn dream reality", self.turnDreamReality)
+        fbox3.addRow("Feel accomplish dream", self.feelAccomplishDream)
+#        fbox3.addRow("Backstory changed everything", self.backstoryChangedEverything)
+        
 
+        grp04.setLayout(fbox3)
+
+        fbox4 = QFormLayout()
+        grp05 = QGroupBox('Backstory scene that changed everything')
+        fbox4.addRow("What they go believing and how they change", self.beliefChange) 
+        fbox4.addRow("How they struggle to hold to old belief", self.struggleHoldBelief) 
+        fbox4.addRow("New belief", self.newBelief) 
+
+        grp05.setLayout(fbox4)
+
+        vbox.addWidget(grp02)
+        vbox.addWidget(grp03)
+        vbox.addWidget(grp04)
+        vbox.addWidget(grp05)
+
+        vbox.addStretch()
+#        self.setLayout(vbox)
+        self.setLayout(vbox)
 
 
 class CharacterListWidget(QWidget):
-    _parent = ''
-    _items = []
     def __init__(self):
         super().__init__()
-        self.setUI()
 
-    def setParent(self, parent):
-        self._parent = parent
-
-    def getItems(self):
-        items = []
-        for item in self._items:
-            items.append(item['item'].serialize())
-        return items
-
-    def export(self):
-        pass
-
-    def setUI(self):
         vbox = QVBoxLayout()
-
         label = QLabel("Characters")
+        label.setAlignment(Qt.AlignCenter)
+        label.setObjectName('CharacterList')
         vbox.addWidget(label)
-        
-        self.table = QTableWidget(1,5)
-        self.columnLabels = ["Character","Role","Age","Gender","hash"]
-        self.table.setHorizontalHeaderLabels(self.columnLabels)
+
+        # CREATE THE TABLE
+        self.table = QTableView(self)  # SELECTING THE VIEW
+        self.table.setGeometry(0, 0, 575, 575)
+        self.model = QStandardItemModel(self)  # SELECTING THE MODEL - FRAMEWORK THAT HANDLES QUERIES AND EDITS
+        self.table.setModel(self.model)  # SETTING THE MODEL
+        self.table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.populate()
+
+        self.table.doubleClicked.connect(self.on_click)
+
         vbox.addWidget(self.table)
-
-
-        with open('save/sample.char') as f:
-            read_data = f.read()
-            data = json.loads(read_data)
-#            print(data)
-            f.close()
-
-        self.model = TableModel(data)
-#        self.table.setModel(self.model)
-#        self.model.setTableData()
-#        self.table.setModel(self.model)
-        self.table.setSortingEnabled(True)
-        self.table.setSelectionMode(QAbstractItemView.SingleSelection)
-        self.table.setSelectionBehavior(QAbstractItemView.SelectRows)
-        self.table.clicked.connect(self.selectRow)
-        self.table.clicked.connect(self.showSelection)
-
-        self.table.setColumnHidden(4, True)
-
+        vbox.addStretch()
         self.setLayout(vbox)
 
-    def selectRow(self, index):
-        print("current row is %d", index.row())
 
-    def showSelection(self, item):
-        cellContent = item.data()
-        print(cellContent)  # test
-        sf = "You clicked on {}".format(cellContent)
-        # display in title bar for convenience
-        self.setWindowTitle(sf)
+    def on_click(self, signal):
+        row = signal.row()  # RETRIEVES ROW OF CELL THAT WAS DOUBLE CLICKED
+        column = signal.column()  # RETRIEVES COLUMN OF CELL THAT WAS DOUBLE CLICKED
+        cell_dict = self.model.itemData(signal)  # RETURNS DICT VALUE OF SIGNAL
+        cell_value = cell_dict.get(0)  # RETRIEVE VALUE FROM DICT
+
+        index = signal.sibling(row, 0)
+        index_dict = self.model.itemData(index)
+        index_value = index_dict.get(0)
+        print(
+            'Row {}, Column {} clicked - value: {}\nColumn 1 contents: {}'.format(row, column, cell_value, index_value))
+
+    def populate(self):
+        # GENERATE A 4x10 GRID OF RANDOM NUMBERS.
+        # VALUES WILL CONTAIN A LIST OF INT.
+        # MODEL ONLY ACCEPTS STRINGS - MUST CONVERT.
+        values = []
+        for i in range(10):
+            sub_values = []
+            for i in range(4):
+                value = random.randrange(1, 100)
+                sub_values.append(value)
+            values.append(sub_values)
+
+        for value in values:
+            row = []
+            for item in value:
+                cell = QStandardItem(str(item))
+                row.append(cell)
+            self.model.appendRow(row)
+
+        self.show()
 
 
-    def load(self):
-        filename, filter = QFileDialog.getOpenFileName(self, 'Open file', '~/')
-        with open(filename, 'r') as json_data:
-            json_data = json.load(json_data)
-            print(json_data)
-            print('*'*80)
-            self._parent.loadData(json_data)
 
 class AppMainWindow(QMainWindow):
     
@@ -146,12 +226,12 @@ class AppMainWindow(QMainWindow):
         self.setCentralWidget(self.tabs)
         
         self._tab01 = QWidget()
-        self._tab02 = QWidget()
+        self._character = CharacterWidget()
         self._tab03 = QWidget()
         self._tab04 = QWidget()
 
         self.tabs.addTab(self._grid,"List Characters")
-        self.tabs.addTab(self._tab02,"Character  - main")
+        self.tabs.addTab(self._character,"Character  - main")
         self.tabs.addTab(self._tab03,"Character - extra")
         self.tabs.addTab(self._tab04,"Reference")
 
